01860b03b33dec1180f9b90646cce9247907b03f
CoderTofu <vioneverforget@gmail.com>
2026-04-26 01:13:56 +0800

    removed search module

--- components/layout/BottomNav.tsx
+++ components/layout/BottomNav.tsx
@@ -22,11 +22,11 @@ const NAV_ITEMS = [
     href: "/home",
     icon: Home,
   },
-  {
-    label: "Search",
-    href: "/search",
-    icon: Search,
-  },
+  // {
+  //   label: "Search",
+  //   href: "/search",
+  //   icon: Search,
+  // },
   {
     label: "Post",
     href: "/post",
